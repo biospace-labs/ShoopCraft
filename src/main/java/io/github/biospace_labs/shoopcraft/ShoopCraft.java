eace40a3b90a407a237814e09665e87d14ae9d73
Spacepuffin <Spacepuffin7@gmaiil.com>
2017-04-16 01:13:26 -0400

    Added "accepable versions"

--- src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
+++ src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
@@ -12,11 +12,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid=ShoopCraft.MODID, name=ShoopCraft.NAME, version=ShoopCraft.VERSION)
+@Mod(modid=ShoopCraft.MODID, name=ShoopCraft.NAME, version=ShoopCraft.VERSION, acceptedMinecraftVersions = ShoopCraft.ACCEPTEDVERSIONS)
 public class ShoopCraft {
     public static final String MODID = "shoopcraft";
     public static final String NAME = "ShoopCraft";
     public static final String VERSION = "1.0";
+    public static final String ACCEPTEDVERSIONS = "[1.11.2]";
 
     @SidedProxy(serverSide = "io.github.biospace_labs.shoopcraft.proxy.CommonProxy", clientSide = "io.github.biospace_labs.shoopcraft.proxy.ClientProxy")
     public static CommonProxy proxy;
