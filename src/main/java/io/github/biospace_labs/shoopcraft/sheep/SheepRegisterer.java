90d2c2270ab0e06990c79e3c2d8ab755c9392990
Spacepuffin <Spacepuffin7@gmaiil.com>
2017-04-16 01:14:51 -0400

    Added Rendersheep and SheepRegisterer (not ready, commented out)

---
+++ src/main/java/io/github/biospace_labs/shoopcraft/sheep/SheepRegisterer.java
@@ -0,0 +1,15 @@
+package io.github.biospace_labs.shoopcraft.sheep;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * Created by Spacepuffin on 2017-04-15.
+ */
+/**
+@Mod.EventBusSubscriber
+public class SheepRegisterer {
+    @SubscribeEvent
+    public static
+}
+*/
