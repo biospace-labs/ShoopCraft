a3279b8bac19460c5d09a4c5fcb9e5667ec35014
Spacepuffin <Spacepuffin7@gmaiil.com>
2017-04-16 13:02:20 -0400

    Rearranging things to keep things organized

--- src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
+++ src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
@@ -19,10 +19,13 @@ public class ShoopCraft {
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
+    public static final String CLIENT_PROXY_CLASS = "io.github.biospace_labs.shoopcraft.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "io.github.biospace_labs.shoopcraft.proxy.ServerProxy";
+
     @Mod.Instance
     public static ShoopCraft instance;
 
-    @SidedProxy(serverSide = "io.github.biospace_labs.shoopcraft.proxy.CommonProxy", clientSide = "io.github.biospace_labs.shoopcraft.proxy.ClientProxy")
+    @SidedProxy(serverSide = ShoopCraft.SERVER_PROXY_CLASS, clientSide = ShoopCraft.CLIENT_PROXY_CLASS)
     public static CommonProxy proxy;
 
     @Mod.EventHandler
