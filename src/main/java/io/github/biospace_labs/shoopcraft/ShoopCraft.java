cbd99734b23d1652ddc39efca89cea174638d7e0
Spacepuffin <Spacepuffin7@gmaiil.com>
2017-04-16 12:50:17 -0400

    Typo

--- src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
+++ src/main/java/io/github/biospace_labs/shoopcraft/ShoopCraft.java
@@ -12,12 +12,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid=ShoopCraft.MODID, name=ShoopCraft.NAME, version=ShoopCraft.VERSION, acceptedMinecraftVersions = ShoopCraft.ACCEPTEDVERSIONS)
+@Mod(modid=ShoopCraft.MODID, name=ShoopCraft.NAME, version=ShoopCraft.VERSION, acceptedMinecraftVersions = ShoopCraft.ACCEPTED_VERSIONS)
 public class ShoopCraft {
     public static final String MODID = "shoopcraft";
     public static final String NAME = "ShoopCraft";
     public static final String VERSION = "1.0";
-    public static final String ACCEPTEDVERSIONS = "[1.11.2]";
+    public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
     @SidedProxy(serverSide = "io.github.biospace_labs.shoopcraft.proxy.CommonProxy", clientSide = "io.github.biospace_labs.shoopcraft.proxy.ClientProxy")
     public static CommonProxy proxy;
